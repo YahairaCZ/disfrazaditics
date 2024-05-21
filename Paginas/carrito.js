9d86dd12a1a7833377f511616b00c2bd1e799c36
Yahaira Zuniga <yahaira.cardona.z@gmail.com>
2024-05-20 23:13:09 -0600

    aja

--- Paginas/carrito.js
+++ Paginas/carrito.js
@@ -1,19 +1,17 @@
 var carrito = localStorage.getItem('carrito');
-if (carrito) {
-    carrito = JSON.parse(carrito);
-    var carritoBody = document.getElementById('carritoBody');
+        if (carrito) {
+            carrito = JSON.parse(carrito);
+            var carritoBody = document.getElementById('carritoBody');
+    
+            carrito.forEach(function(producto, index) {
+                var fila = document.createElement('tr');
+                fila.innerHTML = '<td>' + producto.nombre + '</td><td>' + producto.precio + '</td><td><button onclick="eliminarProducto(' + index + ')">Eliminar</button></td>';
+                carritoBody.appendChild(fila);
+            });
+        }
 
-    // Mostrar cada producto en la lista de deseos
-    carrito.forEach(function(producto, index) {
-        var fila = document.createElement('tr');
-        fila.innerHTML = '<td>' + producto.nombre + '</td><td>' + producto.precio + '</td><td><button onclick="eliminarProducto(' + index + ')">Eliminar</button></td>';
-        carritoBody.appendChild(fila);
-    });
-}
-
-// Función para eliminar un producto de la lista de deseos
-function eliminarProducto(index) {
-    carrito.splice(index, 1);
-    localStorage.setItem('listaDeseos', JSON.stringify(carrito));
-    location.reload(); // Recargar la página para actualizar la lista
-}
+        function eliminarProducto(index) {
+            carrito.splice(index, 1);
+            localStorage.setItem('carrito', JSON.stringify(carrito));
+            location.reload(); 
+        }
